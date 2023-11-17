f2881111c68a3ee451191a08253c87d763c2f205
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-11-17 17:45:07 +0200

    add

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,6 @@
 const { HttpError, CtrlWrapper } = require("../healpers");
 const { ItemModel } = require("../modules/Item");
+const { pagination } = require("../utils/paginations");
 
 const createItem = async (req, res) => {
   if (req.file) {
@@ -18,12 +19,25 @@ const createItem = async (req, res) => {
 };
 
 const getAllItems = async (req, res) => {
-  const items = await ItemModel.find();
+  const { page: currentPage, limit: currentLimit } = req.query;
+  const { page, limit, skip } = pagination(currentPage, currentLimit);
+
+  const items = await ItemModel.find({}, "", { skip, limit });
+  const totalItems = await ItemModel.find().count();
+
   if (!items) {
     throw HttpError(404, "Items not found");
   }
 
-  res.json(items);
+  res.json({
+    items,
+    meta: {
+      page,
+      limit,
+      totalItems,
+      totalPages: Math.ceil(totalItems / limit),
+    },
+  });
 };
 
 const getItemDetails = async (req, res) => {
@@ -70,12 +84,27 @@ const updateItem = async (req, res) => {
 
 const getCategoryItems = async (req, res) => {
   const { categoryName } = req.params;
-  const categoryItems = await ItemModel.find({ category: categoryName });
+  const { page: currentPage, limit: currentLimit } = req.query;
+  const { page, limit, skip } = pagination(currentPage, currentLimit);
+
+  const categoryItems = await ItemModel.find({ category: categoryName }, "", {
+    skip,
+    limit,
+  });
+  const totalItems = await ItemModel.find({ category: categoryName }).count();
   if (!categoryItems) {
     throw HttpError(404, "Items not found");
   }
 
-  res.json(categoryItems);
+  res.json({
+    categoryItems,
+    meta: {
+      totalItems,
+      page,
+      limit,
+      totalPages: Math.ceil(totalItems / limit),
+    },
+  });
 };
 
 module.exports = {
